0590daa25456ee63bc8a0c721b230ab251df2b04
Oleg <olegkuz39@gmail.com>
2023-10-10 14:30:59 +0200

    javaRush 6.8

--- src/Java6_7/Tasc2.java
+++ src/Java6_7/Tasc2.java
@@ -2,5 +2,13 @@ package Java6_7;
 import java.util.Arrays;
 
 public class Tasc2 {
+    public static String[][] strings = new String[][]{{"🙂", "🙂", "🙂", "🙂"}, {"🙃", "🙃", "🙃", "🙃"}, {"😋", "😋", "😋", "😋"}, {"😎", "😎", "😎", "😎"}};
+    public static int[][] ints = new int[][]{{1, 2, 3, 4}, {5, 6, 7, 8}, {9, 10, 11, 12}, {13, 14, 15, 16}};
 
+    public static void main(String[] args) {
+        String str = Arrays.deepToString(strings);
+        String integer = Arrays.deepToString(ints);
+        System.out.println(str);
+        System.out.println(integer);
+    }
 }
