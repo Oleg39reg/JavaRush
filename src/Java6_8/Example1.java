558c3683dddb4c2659a858e2a75be58456c2ba5e
Oleg <olegkuz39@gmail.com>
2023-10-11 18:14:54 +0200

    javaRush 6.8

---
+++ src/Java6_8/Example1.java
@@ -0,0 +1,16 @@
+package Java6_8;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Example1 {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        int[] arr = {3,2,1,8,5,4,7,9,6,10};
+        Arrays.sort(arr);//сортировка массива
+        int target = s.nextInt();//ввод числа с клавиатуры
+        int result = Arrays.binarySearch(arr, target);//бинарный поиск
+        System.out.println("Элемент найден в индексе "+result);
+
+    }
+}
