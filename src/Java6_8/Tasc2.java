04bee4985bdef5a552d4f21cfc77acfee5a637a2
Oleg <olegkuz39@gmail.com>
2023-10-11 13:02:48 +0200

    javaRush 6.8

---
+++ src/Java6_8/Tasc2.java
@@ -0,0 +1,19 @@
+package Java6_8;
+/*
+Реализуй метод main(String[]), который делит массив array на два подмассива и заполняет ими двумерный массив result.
+Если длина массива нечетная, то большую часть нужно скопировать в первый подмассив.
+Для разделения массива используй метод Arrays.copyOfRange(int[], int, int). Порядок элементов не меняй.
+ */
+import java.util.Arrays;
+
+public class Tasc2 {
+    public static int[][] result = new int[2][];
+    public static int[] array = new int[]{1,2,3,4,5,6,7,8,9,10,11};
+
+    public static void main(String[] args) {
+        int firstPart = array.length%2==0?array.length/2: array.length/2+1;
+        result[0]=Arrays.copyOfRange(array,0,firstPart);
+        result[1]=Arrays.copyOfRange(array,firstPart,array.length);
+        System.out.println(Arrays.deepToString(result));
+    }
+}
