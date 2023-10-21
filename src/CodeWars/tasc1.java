f41fafd3d57e0ad7fa06823b79ab5f54cb7e10a0
Oleg <olegkuz39@gmail.com>
2023-10-21 13:18:12 +0200

    javaRush 8.1

---
+++ src/CodeWars/tasc1.java
@@ -0,0 +1,12 @@
+package CodeWars;
+
+public class tasc1 {
+    public static String greet(String name)
+    {
+        return "Hello,"+name+"how are you doing today?";
+    }
+
+    public static void main(String[] args) {
+        greet("Oleg");
+    }
+}
---
+++ src/Java7_4/Tasc1.java
@@ -0,0 +1,16 @@
+package Java7_4;
+
+public class Tasc1 {
+    public static void main(String[] args) {
+        everOrOdd(7);
+    }
+
+    public static void everOrOdd(int number) {
+        if (number % 2 == 0) {
+            System.out.println("Чётное");
+        } else
+            System.out.println("Не чётное");
+    }
+}
+
+
